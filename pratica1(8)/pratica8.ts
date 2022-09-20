215fb5e3bdda07b6cb3b33555b51967d1f2fbcbf
LucasMarques97 <111980558+LucasMarques97@users.noreply.github.com>
2022-09-20 12:25:05 -0300

    Add files via upload

---
+++ pratica1(8)/pratica8.ts
@@ -0,0 +1,82 @@
+//ATIVIDADE 1 Crie uma classe cliente e apresente os atributos e métodos referentes a esta classe, em seguida crie um objeto cliente, defina as instâncias deste objeto e apresente as informações deste objeto no console.
+
+// class cliente {
+//     nome: string
+//     idade: number
+//     sexo: string
+
+//     sinopse(){
+//         console.log(`O cliente ${pedro.nome} do sexo ${pedro.sexo}, possui ${pedro.idade} anos.`)
+// }
+
+// }
+// const pedro = new cliente()
+// pedro.nome = "Pedro Hélio"
+// pedro.idade = 30
+// pedro.sexo = "Masculino"
+
+// pedro.sinopse()
+
+//ATIVIDADE 2 Crie uma classe avião e apresente os atributos e métodos referentes a esta classe, em seguida crie um objeto avião, defina as instâncias deste objeto e apresente as informações deste objeto no console.
+
+// class aviao {
+//     nome: string
+//     Companhia: string
+//     numeroDePessoasSuportadas: number
+//     porte: string
+
+//     sinopse(){
+//         console.log(`O Avião ${hercules.nome} da companhia ${hercules.Companhia}, de ${hercules.porte} Porte, possui capacidade total para ${hercules.numeroDePessoasSuportadas} Soldados.`)
+// }
+
+// }
+// const hercules = new aviao()
+// hercules.nome = "Hercules Tunado"
+// hercules.Companhia = "Exercito Brasileiro"
+// hercules.numeroDePessoasSuportadas = 120
+// hercules.porte = "Grande"
+
+// hercules.sinopse()
+
+//ATIVIDADE 3 Crie uma classe produto eletrônico e apresente os atributos e métodos referentes a esta classe, em seguida crie um objeto produto eletrônico, defina as instâncias deste objeto e apresente as informações deste objeto no console.
+
+// class produtosEletronicos {
+//     modelo: string
+//     marca: string
+//     gigas: number
+//     overclock: string
+
+//     sinopse(){
+//         console.log(`Placa de vídeo ${gtx1080.modelo} da marca ${gtx1080.marca}, possui ${gtx1080.gigas} gigas, e também com capacidade de realizar ${gtx1080.overclock}.`)
+// }
+
+// }
+// const gtx1080 = new produtosEletronicos()
+// gtx1080.modelo = "Gtx 1080 Titan"
+// gtx1080.marca = "Nvidea"
+// gtx1080.gigas = 8
+// gtx1080.overclock = "Overclock"
+
+// gtx1080.sinopse()
+
+//ATIVIDADE 4 Crie uma classe funcionário e apresente os atributos e métodos referentes esta classe, em seguida crie um objeto funcionário, defina as instâncias deste objeto e apresente as informações deste objeto no console.
+
+
+class funcionarios {
+    nome: string
+    idade: number
+    setor: string
+    função: string
+
+    sinopse(){
+        console.log(`O funcionário ${carlos.nome} possui ${carlos.idade} anos de idade, e trabalha no setor de ${carlos.setor} na função de ${carlos.função}.`)
+}
+
+}
+const carlos = new funcionarios()
+carlos.nome = "Carlos Fabuloso"
+carlos.idade = 25
+carlos.setor = "logistica"
+carlos.função = "Auxiliar"
+
+carlos.sinopse()
